1c0b600b256c094c5b9dd966beeb49cb4a711495
cadebo69 <cadebo69@yahoo.es>
2021-10-13 19:38:03 -0500

    Agregamos archivos a git

---
+++ src/main/java/minticgrupo25/proyectoclase/ProyectoclaseApplication.java
@@ -0,0 +1,13 @@
+package minticgrupo25.proyectoclase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoclaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoclaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/minticgrupo25/proyectoclase/ProyectoclaseApplicationTests.java
@@ -0,0 +1,13 @@
+package minticgrupo25.proyectoclase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoclaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
